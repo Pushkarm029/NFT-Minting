77e8d6446fb0240e28426d2a634d478c40940fa0
pushkarm029 <pushkarmishra029@gmail.com>
2024-09-01 19:02:39 +0530

    fix
    Signed-off-by: pushkarm029 <pushkarmishra029@gmail.com>

--- app/client.ts
+++ app/client.ts
@@ -1,16 +1,11 @@
 import * as web3 from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import type { BossBattle } from "../target/types/boss_battle";
+import type { NFT } from "../target/types/nft";
 
 // Configure the client to use the local cluster
 anchor.setProvider(anchor.AnchorProvider.env());
 
-const program = anchor.workspace.BossBattle as anchor.Program<BossBattle>;
-
-const [enemyBossPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("boss")],
-    program.programId
-);
+const program = anchor.workspace.NFT as anchor.Program<NFT>;
 
 async function confirmAndLogTransaction(txHash) {
     const { blockhash, lastValidBlockHeight } =
@@ -29,40 +24,3 @@ async function confirmAndLogTransaction(txHash) {
         `Solana Explorer: https://explorer.solana.com/tx/${txHash}?cluster=devnet`
     );
 }
-
-async function respawnEnemyBoss() {
-    console.log("Respawning Enemy Boss");
-    const tx = await program.methods.respawn().accounts({
-        player: program.provider.publicKey,
-    }).rpc();
-    await confirmAndLogTransaction(tx);
-
-    const enemyBossData = await program.account.enemyBoss.fetch(enemyBossPDA);
-    console.log("Enemy Health: ", enemyBossData.health.toNumber() + "\n");
-}
-
-async function attackLoop() {
-    let enemyBossData;
-
-    try {
-        do {
-            console.log("Attacking Enemy Boss");
-            const tx = await program.methods
-                .attack()
-                .accounts({
-                    player: program.provider.publicKey,
-                    enemyBoss: enemyBossPDA,
-                })
-                .rpc();
-            await confirmAndLogTransaction(tx);
-
-            enemyBossData = await program.account.enemyBoss.fetch(enemyBossPDA);
-            console.log("Enemy Health: ", enemyBossData.health.toNumber() + "\n");
-        } while (enemyBossData.health.toNumber() >= 0);
-    } catch (e) {
-        console.log(e.error.errorMessage);
-    }
-}
-
-// await respawnEnemyBoss();
-// await attackLoop();

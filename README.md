53c372afaef2720716b86b2482589c6a9e90f8b2
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-10-28 16:18:59 +0530

    2/3 tests passing
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# List of commands:
+
+```bash
+solana-test-validator
+
+anchor test --skip-local-validator
+
+solana program dump --url mainnet-beta metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s metadata.so
+
+rm -rf test-ledger
+
+solana-test-validator --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s metadata.so
+
+solana-test-validator --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s metadata.so --reset
+```
--- tests/test.ts
+++ tests/test.ts
@@ -1,13 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaNftMint } from "../target/types/solana_nft_mint";
+import { AccountLayout } from '@solana/spl-token';
 import {
   TOKEN_PROGRAM_ID,
   ASSOCIATED_TOKEN_PROGRAM_ID,
   createAssociatedTokenAccountInstruction,
   getAssociatedTokenAddress,
 } from "@solana/spl-token";
-// import { PROGRAM_ID as METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
 import { expect } from "chai";
 import { PublicKey } from "@solana/web3.js";
 
@@ -18,10 +18,10 @@ describe("nft", () => {
   const program = anchor.workspace.SolanaNftMint as Program<SolanaNftMint>;
   const wallet: anchor.Wallet = provider.wallet as anchor.Wallet;
 
-  let mintKeypair: anchor.web3.Keypair;
   let tokenAddress: anchor.web3.PublicKey;
   let metadataAddress: anchor.web3.PublicKey;
   let masterEditionAddress: anchor.web3.PublicKey;
+  let mintPDA: anchor.web3.PublicKey;
 
   const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
     "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
@@ -32,41 +32,47 @@ describe("nft", () => {
   const NFT_URI = "https://example.com/my-cool-nft";
 
   before(async () => {
-    mintKeypair = anchor.web3.Keypair.generate();
+    // Get the PDA for the mint account
+    [mintPDA] = PublicKey.findProgramAddressSync(
+      [Buffer.from("mint")],
+      program.programId
+    );
 
+    // Derive the metadata account address
     metadataAddress = PublicKey.findProgramAddressSync(
       [
         Buffer.from("metadata"),
         TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-        mintKeypair.publicKey.toBuffer(),
+        mintPDA.toBuffer(),
       ],
       TOKEN_METADATA_PROGRAM_ID
     )[0];
 
+    // Derive the master edition account address
     masterEditionAddress = PublicKey.findProgramAddressSync(
       [
         Buffer.from("metadata"),
         TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-        mintKeypair.publicKey.toBuffer(),
+        mintPDA.toBuffer(),
         Buffer.from("edition"),
       ],
       TOKEN_METADATA_PROGRAM_ID
     )[0];
 
+    // Get the associated token account address
     tokenAddress = await getAssociatedTokenAddress(
-      mintKeypair.publicKey,
+      mintPDA,
       wallet.publicKey
     );
+
+    console.log("Mint PDA:", mintPDA.toBase58());
+    console.log("Metadata Address:", metadataAddress.toBase58());
+    console.log("Master Edition Address:", masterEditionAddress.toBase58());
+    console.log("Token Address:", tokenAddress.toBase58());
   });
 
   it("Mints an NFT", async () => {
-    const mintPDA = PublicKey.findProgramAddressSync(
-      [Buffer.from("mint")],
-      program.programId
-    )[0];
-
-    console.log("mintPDA", mintPDA.toBase58());
-    await program.methods
+    const tx = await program.methods
       .mintNft(NFT_NAME, NFT_SYMBOL, NFT_URI)
       .accounts({
         admin: wallet.publicKey,
@@ -82,60 +88,127 @@ describe("nft", () => {
       })
       .rpc();
 
-      const mintAccount = await program.provider.connection.getAccountInfo(mintPDA);
-      if (!mintAccount) {
-        console.log("PDA is not initialized");
-      } else {
-        console.log("PDA initialized", mintAccount.owner.toBase58());
-      }
-      
+    console.log("Transaction signature:", tx);
+
+    // Verify the mint account was created
+    const mintAccount = await program.provider.connection.getAccountInfo(mintPDA);
+    expect(mintAccount).to.not.be.null;
+
+    // Verify the token account was created
     const tokenAccount = await program.provider.connection.getAccountInfo(tokenAddress);
     expect(tokenAccount).to.not.be.null;
   });
 
-  // it("Transfers the NFT", async () => {
-  //   const recipient = anchor.web3.Keypair.generate();
-  //   const recipientTokenAddress = await getAssociatedTokenAddress(
-  //     mintKeypair.publicKey,
-  //     recipient.publicKey
+  it("Transfers the NFT", async () => {
+    const recipient = anchor.web3.Keypair.generate();
+    const recipientTokenAddress = await getAssociatedTokenAddress(
+      mintPDA,
+      recipient.publicKey
+    );
+
+    // Transfer the NFT
+    await program.methods
+      .transferNft()
+      .accounts({
+        sender: wallet.publicKey,
+        senderNftTokenAccount: tokenAddress,
+        recipientNftTokenAccount: recipientTokenAddress,
+        recipient: recipient.publicKey,
+        tokenMint: mintPDA,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .rpc();
+
+    // Check that the recipient's token account now has the NFT
+    const recipientTokenAccountInfo = await program.provider.connection.getAccountInfo(recipientTokenAddress);
+    expect(recipientTokenAccountInfo).to.not.be.null;
+
+    // Decode the recipient token account data
+    const recipientTokenAccountData = AccountLayout.decode(recipientTokenAccountInfo.data);
+    expect(recipientTokenAccountData.amount.toString()).to.not.equal('0'); // Expecting some amount
+
+    // Verify that the sender's token account no longer holds the NFT
+    const senderTokenAccountInfo = await program.provider.connection.getAccountInfo(tokenAddress);
+    expect(senderTokenAccountInfo).to.not.be.null; // It should still be not null
+
+    // Decode the sender token account data
+    const senderTokenAccountData = AccountLayout.decode(senderTokenAccountInfo.data);
+    expect(senderTokenAccountData.amount.toString()).to.equal('0'); // Should be 0 after transfer
+  });
+
+  // it("Mints a new NFT to burn", async () => {
+  //   // Create a new mint PDA for the new NFT
+  //   const [newMintPDA] = PublicKey.findProgramAddressSync(
+  //     [Buffer.from("mint")],
+  //     program.programId
   //   );
 
-  //   await program.methods
-  //     .transferNft()
+  //   // Derive a new associated token address and metadata for the new NFT
+  //   const newTokenAddress = await getAssociatedTokenAddress(
+  //     newMintPDA,
+  //     wallet.publicKey
+  //   );
+  //   const newMetadataAddress = PublicKey.findProgramAddressSync(
+  //     [
+  //       Buffer.from("metadata"),
+  //       TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+  //       newMintPDA.toBuffer(),
+  //     ],
+  //     TOKEN_METADATA_PROGRAM_ID
+  //   )[0];
+
+  //   const newMasterEditionAddress = PublicKey.findProgramAddressSync(
+  //     [
+  //       Buffer.from("metadata"),
+  //       TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+  //       newMintPDA.toBuffer(),
+  //       Buffer.from("edition"),
+  //     ],
+  //     TOKEN_METADATA_PROGRAM_ID
+  //   )[0];
+
+  //   const tx = await program.methods
+  //     .mintNft("SSDS", "SSS", "SSS.com/example")
   //     .accounts({
-  //       sender: wallet.publicKey,
-  //       senderNftTokenAccount: tokenAddress,
-  //       recipientNftTokenAccount: recipientTokenAddress,
-  //       recipient: recipient.publicKey,
-  //       tokenMint: mintKeypair.publicKey,
+  //       admin: wallet.publicKey,
+  //       tokenMint: newMintPDA,
+  //       associatedTokenAccount: newTokenAddress,
+  //       metadataAccount: newMetadataAddress,
+  //       masterEditionAccount: newMasterEditionAddress,
   //       tokenProgram: TOKEN_PROGRAM_ID,
   //       associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+  //       tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
   //       systemProgram: anchor.web3.SystemProgram.programId,
   //       rent: anchor.web3.SYSVAR_RENT_PUBKEY,
   //     })
-  //     .preInstructions([
-  //       createAssociatedTokenAccountInstruction(
-  //         wallet.publicKey,
-  //         recipientTokenAddress,
-  //         recipient.publicKey,
-  //         mintKeypair.publicKey
-  //       ),
-  //     ])
   //     .rpc();
 
-  //   const recipientTokenAccount = await program.provider.connection.getAccountInfo(recipientTokenAddress);
-  //   expect(recipientTokenAccount).to.not.be.null;
+  //   console.log("Transaction signature for new NFT:", tx);
 
-  //   const senderTokenAccount = await program.provider.connection.getAccountInfo(tokenAddress);
-  //   expect(senderTokenAccount).to.be.null;
+  //   // Verify the mint account was created
+  //   const mintAccount = await program.provider.connection.getAccountInfo(newMintPDA);
+  //   expect(mintAccount).to.not.be.null;
+
+  //   // Verify the token account was created
+  //   const tokenAccount = await program.provider.connection.getAccountInfo(newTokenAddress);
+  //   expect(tokenAccount).to.not.be.null;
   // });
 
-  // it("Burns the NFT", async () => {
+  // it("Burns the newly minted NFT", async () => {
+  //   // Setup: Ensure the new NFT token account has a balance of 1
+  //   const newTokenAccountInfo = await program.provider.connection.getAccountInfo(tokenAddress);
+  //   const newTokenAccountData = AccountLayout.decode(newTokenAccountInfo.data);
+  //   expect(newTokenAccountData.amount.toString()).to.equal('1');
+
+  //   // Set up the burn method call
   //   await program.methods
   //     .burnNft()
   //     .accounts({
   //       admin: wallet.publicKey,
-  //       tokenMint: mintKeypair.publicKey,
+  //       tokenMint: mintPDA,
   //       nftTokenAccount: tokenAddress,
   //       metadataAccount: metadataAddress,
   //       masterEditionAccount: masterEditionAddress,
@@ -147,10 +220,19 @@ describe("nft", () => {
   //     })
   //     .rpc();
 
-  //   const mintAccount = await program.provider.connection.getAccountInfo(mintKeypair.publicKey);
-  //   expect(mintAccount).to.be.null;
+  //   // Verify the mint account has been burned
+  //   const mintAccount = await program.provider.connection.getAccountInfo(mintPDA);
+  //   expect(mintAccount).to.be.null; // The mint account should no longer exist
 
+  //   // Verify the token account has been burned
   //   const tokenAccount = await program.provider.connection.getAccountInfo(tokenAddress);
-  //   expect(tokenAccount).to.be.null;
+  //   expect(tokenAccount).to.be.null; // The associated token account should also no longer exist
+
+  //   // Verify that the metadata and master edition accounts are still valid (optional)
+  //   const metadataAccountInfo = await program.provider.connection.getAccountInfo(metadataAddress);
+  //   expect(metadataAccountInfo).to.not.be.null; // Metadata account should still exist
+
+  //   const masterEditionAccountInfo = await program.provider.connection.getAccountInfo(masterEditionAddress);
+  //   expect(masterEditionAccountInfo).to.not.be.null; // Master edition account should still exist
   // });
 });
